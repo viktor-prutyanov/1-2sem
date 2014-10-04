73ad874bb55404f141e77ed435c7529cf0542f6e
Viktor Prutyanov <vitteran@gmail.com>
2014-10-05 03:49:30 +0400

    added reversed sort

--- Eugene Onegin/Eugene Onegin/main.c
+++ Eugene Onegin/Eugene Onegin/main.c
@@ -14,6 +14,10 @@
 #include <assert.h>
 #include <string.h>
 
+#ifdef _DEBUG
+    #include <Windows.h>
+#endif
+
 /**
     @brief Function that compares two strings presented by pointers
 
@@ -25,6 +29,59 @@
 int str_ptr_cmp(const void *a, const void *b)
 {
     return strcmp (*(char **)a, *(char **)b);
+}
+
+
+/**
+    @brief Function that compares two strings presented by pointers in reversed order
+
+    @param a is first pointer to string
+    @param b is second pointer to string
+
+    @return -1 if a < b, 0 if a = b, 1 if a > b  
+*/
+int str_ptr_cmp_rev(const void *a, const void *b)
+{
+    char *s1 = *(char **)a; 
+    char *s2 = *(char **)b;
+
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+    int len = 0;
+
+    if (len1 >  len2)
+    {
+        len = len2;
+    }
+    else
+    {
+        len = len1;
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s1[len1 - 1 - i] > s2[len2 - 1 -i])
+        {
+            return 1;
+        }
+        else if (s1[len1 - 1 - i] < s2[len2 - 1 -i])
+        {
+            return -1;
+        }
+    }
+
+    if (len1 > len2)
+    {
+        return 1;
+    }
+    else if (len1 < len2)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 /**
@@ -65,7 +122,6 @@ unsigned long int change_n_to_0(char *str, unsigned long int len)
     return lines;
 }
 
-
 /**
     @brief Function that outputs array of pointes to string to file
 
@@ -87,17 +143,31 @@ void str_array_to_file(unsigned long int lines, char **text, FILE *file)
 
 int main()
 {
+    #ifdef _DEBUG
+        LARGE_INTEGER time0 = { }, time1 = { };
+        QueryPerformanceCounter(&time0);
+    #endif
+
     FILE *in_file = fopen ("..\\eugene_onegin.txt", "r");
+
     assert (in_file != nullptr);
     unsigned long int length = file_length(in_file); //unsigned long int length = _filelength(_fileno(file)) + 1;
-    char *buf = (char *)calloc (length, sizeof(char));
+    char *buf = (char *)calloc (length + 1, sizeof(char));
     assert (buf != nullptr);
     unsigned long int len = fread (buf, sizeof(char), length, in_file);
     fclose (in_file);
 
+    printf("In which order do you want to sort Eugene Onegin? Enter 1(direct) or -1(reverse):\n");
+    int order = 0;
+    scanf("%d", &order);
+    assert(order == 1 || order == -1);
+    order == 1 ? printf("You choice is direct order.\n") : printf("You choice is reverse order.\n");
+
     printf ("File length is %u symbols.\nReaded %u symbols.\n", length, len);
     
-    unsigned long int lines = change_n_to_0 (buf, len); 
+    buf[len] = '\n';
+
+    unsigned long int lines = change_n_to_0 (buf, len + 1); 
 
     printf ("Readed %u lines.\n", lines);
 
@@ -107,10 +177,10 @@ int main()
 
     unsigned long int pos = 1;
 
-    for (int i = 0; i < len - 1; i++)
+    for (int i = 0; i < len; i++)
     {
-        assert (0 <= i && i < len);
-        assert (0 <= i + 1 && i + 1 < len);
+        assert (0 <= i && i < len + 1);
+        assert (0 <= i + 1 && i + 1 < len + 1);
         if (buf[i] == '\0')
         {
             assert(0 <= pos && pos < lines + 1);
@@ -121,7 +191,7 @@ int main()
 
     printf ("Pointers created for %u strings.\n", pos);
 
-    qsort (text, lines, sizeof(char *), str_ptr_cmp); 
+    order == 1 ? qsort (text, lines, sizeof(char *), str_ptr_cmp) : qsort (text, lines, sizeof(char *), str_ptr_cmp_rev); 
 
     FILE *out_file = fopen ("..\\eugene_onegin_sorted.txt", "w");
     str_array_to_file(lines, text, out_file);
@@ -132,10 +202,15 @@ int main()
     buf = nullptr;
     free (text);
     text = nullptr;
+    
+    #ifdef _DEBUG
+        QueryPerformanceCounter(&time1);
+        printf("%d\n", time1.QuadPart - time0.QuadPart);
+    #endif
 
     #ifdef _DEBUG
         system("pause");
     #endif
-
+  
     return 0;
 }
