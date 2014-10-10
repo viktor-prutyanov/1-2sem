e8d612c904638f982bd05b56645c9a4ead5a0c6b
Viktor Prutyanov <vitteran@gmail.com>
2014-10-10 19:31:53 +0400

    more functions

--- Eugene Onegin/Eugene Onegin/main.c
+++ Eugene Onegin/Eugene Onegin/main.c
@@ -48,22 +48,15 @@ int str_ptr_cmp_rev(const void *a, const void *b);
 unsigned long int file_length(FILE *file);
 
 /**
-    @brief Function changes symols of newline to 0
-
-    @param str is string to change
-
-    @return len is length of string
-*/
-unsigned long int change_n_to_0(char *str, unsigned long int len);
-
-/**
     @brief Function that outputs array of pointes to string to file
 
     @param lines is number of lines
     @param text is array of pointers
     @param file is output file
+
+    @return 0 if everything is OK, 1 if error exist
 */
-void str_array_to_file(unsigned long int lines, char **text, FILE *file);
+int str_array_to_file(unsigned long int lines, char **text, FILE *file);
 
 /**
     @brief Function that writes string divided by null to array of pointers to string
@@ -75,20 +68,23 @@ void str_array_to_file(unsigned long int lines, char **text, FILE *file);
 
     @return amount of lines that was writed
 */
-long unsigned int str_to_array(char **text, char *buf, long unsigned int len, long unsigned int lines);
+char *read_file_to_string(char filename[], long unsigned int *length, long unsigned int *len);
 
 /**
-    @brief Function that reads file to string divided by newlines
-    @warning length and len can be inequal, for example, if you use Windows file endings (CR LF).
-
+    @brief Function that reads file to array of strings
+    @warning length and read_length can be inequal, for example, if you use Windows file endings (CR LF).
 
     @param filename is address of file
-    @param len is pointer where will be write amount of symbols readed to string
-    @param length is pointer where will be write amount of strings that contains in file
+    @param read_length is a pointer where will be write amount of symbols readed to string
+    @param length is a pointer where will be write amount of strings that contains in file
+    @param lines is a pointer where will be write amount of lines
+    @param buf is a address of text in memory
 
-    @return amount of lines that was writed
+    @return array of pointers to strings 
 */
-char *read_file_to_string(char filename[], long unsigned int *length, long unsigned int *len);
+char **read_strings_from_file(char filename[], long unsigned int *length, long unsigned int *len, long unsigned int *lines, char *buf);
+
+int write_strings_to_file(char filename[], long unsigned int lines, char **text);
 
 int main()
 {
@@ -97,38 +93,48 @@ int main()
         QueryPerformanceCounter (&time0);
     #endif
 
-    printf("In which order do you want to sort Eugene Onegin? Enter (1)direct or (0)reverse\n");
+    printf ("In which order do you want to sort Eugene Onegin? Enter (1)direct or (0)reverse\n");
     int order = -1;
-    scanf("%d", &order);
-    assert(order == 1 || order == 0);
+    scanf ("%d", &order);
+    if (order != 1 && order != 0) 
+    {
+        printf("Incorrect order.\n");
+        #ifdef _DEBUG
+            system ("pause");
+        #endif
+        return 0;
+    }
     printf ("Your choice is %s order.\n", order ? "direct" : "reverse");
 
-    long unsigned int read_length = 0;
-    long unsigned int length = 0;
-    char *buf = read_file_to_string("..\\eugene_onegin.txt", &length, &read_length);
+    long unsigned int read_length = 0, length = 0, lines = 0;
+    char **text;
+    char *buf = nullptr;
+    text = read_strings_from_file("..\\eugene_onegin.txt", &length, &read_length, &lines, buf);
+    if (text == nullptr) 
+    {
+        printf ("Reading failed.\n");
+        #ifdef _DEBUG
+            system ("pause");
+        #endif
+        return 0;
+    }
+    printf ("There are %u symbols in file. Read %u symbols. Read %u lines.\n", length, read_length, lines);
 
-    printf ("File length is %u symbols.\nRead %u symbols.\n", length, read_length);
+    qsort (text, lines, sizeof(*text), order ? str_ptr_cmp : str_ptr_cmp_rev);
 
-    unsigned long int lines = change_n_to_0 (buf, read_length + 1); 
+    if (write_strings_to_file("..\\eugene_onegin_sorted.txt", lines, text)) 
+    {
+        printf ("Writing failed.\n");
+        #ifdef _DEBUG
+            system ("pause");
+        #endif
+        return 0;
+    }
 
-    printf ("Read %u lines.\n", lines);
-
-    char **text = (char**)calloc (lines + 1, sizeof(*text));
-
-    printf ("Pointers created for %u strings.\n", str_to_array(text, buf, read_length, lines));
-
-    qsort (text, lines, sizeof(*text), order ? str_ptr_cmp : str_ptr_cmp_rev );
-
-    FILE *out_file = fopen ("..\\eugene_onegin_sorted.txt", "w");
-    assert (out_file != nullptr);
-    str_array_to_file (lines, text, out_file);
-    fclose (out_file);
-    out_file = nullptr;
-
-    free (buf);
-    buf = nullptr;
     free (text);
     text = nullptr;
+    free (buf);
+    buf = nullptr;
     
     #ifdef _DEBUG
         QueryPerformanceCounter (&time1);
@@ -136,7 +142,7 @@ int main()
     #endif
 
     #ifdef _DEBUG
-        system("pause");
+        system ("pause");
     #endif
   
     return 0;
@@ -195,67 +201,88 @@ unsigned long int file_length(FILE *file)
     return length;
 }
 
-unsigned long int change_n_to_0(char *str, unsigned long int len)
+int str_array_to_file(unsigned long int lines, char **text, FILE *file)
 {
-    unsigned long int lines = 0;
-
-    for (int i = 0; i < len; i++)
-    {
-        assert (0 <= i && i < len);
-        if (str[i] == '\n') 
-        {
-            str[i] = '\0';
-            lines++;
-        }
-        else if (!str[i])
-        {
-            lines++;
-        }
-    }
-    return lines;
-}
-
-void str_array_to_file(unsigned long int lines, char **text, FILE *file)
-{
+    if ((file == nullptr) || (text == nullptr)) return 1;
     for (int i = 0; i < lines; i++)
     {
         assert (0 <= i && i < lines);
         if (*text[i])
         {
+            #ifdef _DEBUG
+                fprintf (file, "|%s|\n", text[i]);
+            #endif
             fprintf (file, "%s\n", text[i]);
         }
     }
+    return 0;
 }
 
-long unsigned int str_to_array(char **text, char *buf, long unsigned int len, long unsigned int lines)
-{
-    text[0] = &(buf[0]);
-
-    long unsigned int pos = 1;
-
-    for (int i = 0; i < len; i++)
-    {
-        assert (0 <= i && i < len + 1);
-        assert (0 <= i + 1 && i + 1 < len + 1);
-        if (buf[i] == '\0')
-        {
-            assert(0 <= pos && pos < lines + 1);
-            text[pos] = &(buf[i + 1]);
-            pos++;
-        }
-    }
-    return pos;
-}
-
-char *read_file_to_string(char filename[], long unsigned int *length, long unsigned int *len)
+char *read_file_to_string(char filename[], long unsigned int *length, long unsigned int *read_length)
 {
     if (filename == nullptr) return nullptr;
     FILE *in_file = fopen (filename, "r");
     if (in_file == nullptr) return nullptr;
-    *length = file_length(in_file); //unsigned long int length = _filelength(_fileno(file)) + 1;
+    *length = file_length (in_file); //unsigned long int length = _filelength(_fileno(file)) + 1;
     char *buf = (char *)calloc (*length + 1, sizeof(char));
     if (buf == nullptr) return nullptr;
-    *len = fread (buf, sizeof(char), *length, in_file);
+    *read_length = fread (buf, sizeof(char), *length, in_file);
     fclose (in_file);
     return buf;
+}
+
+char **read_strings_from_file(char filename[], long unsigned int *length, long unsigned int *read_length, long unsigned int *lines, char *buf)
+{
+    buf = read_file_to_string (filename, length, read_length);
+
+    for (int i = 0; i < *read_length + 1; i++)
+    {
+        assert (0 <= i && i < *read_length + 1);
+        if (buf[i] == '\n') 
+        {
+            buf[i] = '\0';
+            (*lines)++;
+        }
+        else if (!buf[i])
+        {
+            (*lines)++;
+        }
+    }
+
+    char **text = (char**)calloc (*lines + 1, sizeof(*text));
+
+    text[0] = &(buf[0]);
+
+    long unsigned int pos = 1;
+
+    for (int i = 0; i < *read_length; i++)
+    {
+        assert (0 <= i && i < *read_length + 1);
+        assert (0 <= i + 1 && i + 1 < *read_length + 1);
+        if (buf[i] == '\0')
+        {
+            assert(0 <= pos && pos < *lines + 1);
+            text[pos] = &(buf[i + 1]);
+            pos++;
+        }
+    }
+
+    if (*lines == pos)
+    {
+        return text;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+int write_strings_to_file(char filename[], long unsigned int lines, char **text)
+{
+    FILE *out_file = fopen (filename, "w");
+    if (out_file == nullptr) return 1;
+    if (str_array_to_file (lines, text, out_file)) return 1;
+    fclose (out_file);
+    out_file = nullptr;
+    return 0;
 }
