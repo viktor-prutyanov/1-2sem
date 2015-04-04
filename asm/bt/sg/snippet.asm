7c090d987e867f0eea03b800ec96f6fd3ca0d214
vvvv32vvvv <vvvv32vvvv@gmail.com>
2015-04-04 03:50:57 +0300

    BT completed for integers

--- asm/bt/sg/snippet.asm
+++ asm/bt/sg/snippet.asm
@@ -1,7 +1,46 @@
-bits 64                         
-;OUT: number will be pushed and print to stdout
-    mov     rax, 0x01           ; sys_write
-    mov     rdi, 1              ; file descriptor 1 (stdout)
-    mov     rsi, rbp            ; pointer to the data
-    mov     rdx, rcx            ; amount of data
-    syscall                     ; call to the kernel
+; section .text
+; global _start
+; _start:
+bits 64
+    push    rbp
+    mov     rbp, rsp    
+
+    sub     rbp, 32
+
+    mov     rdx, 32             ;message length
+    mov     rsi, rbp            ;message to read
+    mov     rdi, 0              ;file descriptor (stdout)
+    mov     rax, 0              ;system call number (sys_read)
+    syscall
+
+    mov     r9, rbp
+    dec     r9
+    mov     rcx, rbp
+    add     rcx, rax
+    sub     rcx, 2
+    mov     r10, 1
+    mov     rbx, 10
+    xor     rax, rax
+    xor     r8, r8
+
+next:
+    xor     rax, rax
+    mov     al, byte [rcx]
+    sub     al, '0'
+    mul     r10
+    add     r8, rax
+
+    mov     rax, r10
+    mul     rbx
+    mov     r10, rax
+
+    dec     rcx
+    cmp     rcx, r9
+    jne     next
+
+    pop     rbp
+
+    push    r8
+    ; mov     rdi, 0
+    ; mov     rax, 0x3c           ;system call number (sys_exit)
+    ; syscall                     ;call kernel
